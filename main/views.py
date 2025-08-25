4ee102d2e2133cad63e60916cf996272c417090a
eka-tera <kat35546@mail.ru>
2025-08-25 21:51:17 +0300

    добавила главную страницу со случайной цитатой
    Что сделано:
    - создала views для отображения случайной цитаты
    - добавила счетчик просмотров
    - сделала шаблон для отображения цитаты и просмотров
    - настроила urls для главной страницы

--- main/views.py
+++ main/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render     # функция для отображения шаблонов
+import random
+from .models import Quote
 
-# Create your views here.
+
+def home(request):
+    quotes = list(Quote.objects.all())
+    random_quote = random.choice(quotes) if quotes else None
+
+    if random_quote:
+        random_quote.views += 1
+        random_quote.save()
+
+    return render(request, 'main/home.html', {'quote': random_quote})
