b0a16bf750948f777da0e92d8553751ce751c7d1
eka-tera <kat35546@mail.ru>
2025-08-26 17:58:22 +0300

    добавила страница популярных цитат
    Что сделано:
    - создана функция отображения топ-10 цитат по рейтингу
    - реализована сортировка по популярности ( = лайки - дизлайки)
    - добавлен шаблон с красивым оформлением рейтинга
    - настроен url путь для новой страницы
    - добавлена ссылка на страницу топа цитат на главной странице

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('like/<int:quote_id>/', views.like_quote, name='like_quote'),
     path('dislike/<int:quote_id>/', views.dislike_quote, name='dislike_quote'),
     path('add/', views.add_quote, name='add_quote'),
+    path('popular/', views.popular_quotes, name='popular_quotes'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.db import models
 import random
 from .models import Quote
 from .forms import QuoteForm
 
 
 def home(request):
-    # проверка, запрошена ли конкретная цитата через параметр url
     quote_id = request.GET.get('show_quote')
 
-    if quote_id:        # показывается указанная цитату без увеличения счетчика просмотров
+    if quote_id:
         quote = get_object_or_404(Quote, id=quote_id)
-    else:       # выбирается случайная цитата с учетом веса и увеличивается счетчик просмотров
+    else:
         quotes = list(Quote.objects.all())
         if quotes:
             weights = [q.weight for q in quotes]
@@ -23,21 +23,21 @@ def home(request):
     return render(request, 'main/home.html', {'quote': quote})
 
 
-def like_quote(request, quote_id):      # счетчик лайков увеличивается и остаётся та же цитата
+def like_quote(request, quote_id):
     quote = get_object_or_404(Quote, id=quote_id)
     quote.likes += 1
     quote.save()
     return redirect(f'/?show_quote={quote_id}')
 
 
-def dislike_quote(request, quote_id):      # аналогично с дизлайками
+def dislike_quote(request, quote_id):
     quote = get_object_or_404(Quote, id=quote_id)
     quote.dislikes += 1
     quote.save()
     return redirect(f'/?show_quote={quote_id}')
 
 
-def add_quote(request):     # добавление новых цитат в общий пул из интерфейса приложения
+def add_quote(request):
     if request.method == 'POST':
         form = QuoteForm(request.POST)
         if form.is_valid():
@@ -47,3 +47,11 @@ def add_quote(request):     # добавление новых цитат в об
         form = QuoteForm()
 
     return render(request, 'main/add_quote.html', {'form': form})
+
+
+def popular_quotes(request):
+    quotes = Quote.objects.annotate(
+        popularity=models.F('likes') - models.F('dislikes')
+    ).order_by('-popularity')[:10]
+
+    return render(request, 'main/popular_quotes.html', {'quotes': quotes})
