9335aa6a3215a50d74cf32fc6b085fb04c1e0361
eka-tera <kat35546@mail.ru>
2025-08-26 10:25:53 +0300

    добавила систему лайков/дизлайков и вероятностный выбор по весу Что сделано: - Реализован выбор цитат с учетом веса - Добавлены обработчики лайков и дизлайков - В интерфейс добавлены кнопки для оценки цитаты - Настроены url-пути для обработки действий

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('like/<int:quote_id>/', views.like_quote, name='like_quote'),
+    path('dislike/<int:quote_id>/', views.dislike_quote, name='dislike_quote'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,14 +1,30 @@
-from django.shortcuts import render     # функция для отображения шаблонов
+from django.shortcuts import render, redirect, get_object_or_404
 import random
 from .models import Quote
 
 
 def home(request):
     quotes = list(Quote.objects.all())
-    random_quote = random.choice(quotes) if quotes else None
-
-    if random_quote:
+    if quotes:
+        weights = [q.weight for q in quotes]
+        random_quote = random.choices(quotes, weights=weights, k=1)[0]
         random_quote.views += 1
         random_quote.save()
+    else:
+        random_quote = None
 
     return render(request, 'main/home.html', {'quote': random_quote})
+
+
+def like_quote(request, quote_id):
+    quote = get_object_or_404(Quote, id=quote_id)
+    quote.likes += 1
+    quote.save()
+    return redirect('home')
+
+
+def dislike_quote(request, quote_id):
+    quote = get_object_or_404(Quote, id=quote_id)
+    quote.dislikes += 1
+    quote.save()
+    return redirect('home')
