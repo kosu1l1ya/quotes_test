2b64f7b75a84fd8b08cf6a695f21dc0d7483e289
eka-tera <kat35546@mail.ru>
2025-08-27 11:48:13 +0300

    расширила функционал дашборда статистики
    Что сделано:
    - добавлена сортировка статистики по авторам (имя, количество цитат, лайки, просмотры)
    - реализованы кликабельные кнопки и заголовки таблицы для сортировки
    - добавлен топ-3 самых популярных источников по лайкам
    - включена 1 цитата с самой высокой вовлеченностью(лайки/просмотры * 100%)

--- main/views.py
+++ main/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.db import models
+from django.db.models import Count, Sum, F, ExpressionWrapper, FloatField
 import random
 from .models import Quote, Source
 from .forms import QuoteForm
-from django.db.models import Sum
 
 
 def home(request):
@@ -52,7 +51,7 @@ def add_quote(request):
 
 def popular_quotes(request):
     quotes = Quote.objects.annotate(
-        popularity=models.F('likes') - models.F('dislikes')
+        popularity=F('likes') - F('dislikes')
     ).order_by('-popularity')[:10]
 
     return render(request, 'main/popular_quotes.html', {'quotes': quotes})
@@ -64,10 +63,46 @@ def dashboard(request):
     total_views = Quote.objects.aggregate(total=Sum('views'))['total'] or 0
     total_likes = Quote.objects.aggregate(total=Sum('likes'))['total'] or 0
 
+    top_sources = Source.objects.annotate(
+        total_likes=Sum('quotes__likes')
+    ).order_by('-total_likes')[:3]
+
+    top_quote = Quote.objects.annotate(
+        engagement=ExpressionWrapper(
+            F('likes') * 100.0 / F('views'),
+            output_field=FloatField()
+        )
+    ).order_by('-engagement').first()
+
+    # Получаем параметр сортировки из GET-запроса
+    sort_by = request.GET.get('sort', 'quote_count')
+
+    # Определяем порядок сортировки
+    if sort_by == 'name':
+        order_by = 'name'
+    elif sort_by == 'quote_count':
+        order_by = '-quote_count'
+    elif sort_by == 'likes':
+        order_by = '-total_likes'
+    elif sort_by == 'views':
+        order_by = '-total_views'
+    else:
+        order_by = '-quote_count'
+
+    authors_stats = Source.objects.annotate(
+        quote_count=Count('quotes'),
+        total_likes=Sum('quotes__likes'),
+        total_views=Sum('quotes__views'),
+    ).order_by(order_by)[:10]
+
     context = {
         'total_quotes': total_quotes,
         'total_authors': total_authors,
         'total_views': total_views,
         'total_likes': total_likes,
+        'top_sources': top_sources,
+        'top_quote': top_quote,
+        'authors_stats': authors_stats,
+        'current_sort': sort_by,
     }
     return render(request, 'main/dashboard.html', context)
